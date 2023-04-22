f07f6c3089599d7f7a8bc589db512313a180aaa4
sfqwo <marmel.ad.ova@mail.ru>
2023-04-22 23:10:11 +0300

    add footer

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import InstagramSvg from '@assets/icons/Instagram.svg';
+import TelegramSvg from '@assets/icons/Telegram.svg';
+import VKSvg from '@assets/icons/Vk.svg';
+import WhatsappSvg from '@assets/icons/Whatsapp.svg';
+import YoutubeSvg from '@assets/icons/Youtube.svg';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
   return(
-    <>
-      FOOTER
-    </>
+    <div className={styles.footer}>
+      <div className={styles.footer__links}>
+        <TelegramSvg />
+        <WhatsappSvg />
+        <VKSvg />
+        <InstagramSvg />
+        <YoutubeSvg />
+      </div>
+    </div>
   )
 }
 export default Footer;
